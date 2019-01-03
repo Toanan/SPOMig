6bb8f9cc07c4c27e28662799339bde1f524eb491
Antoine Galland <toanan31@gmail.com>
2019-01-03 15:40:54 +0100

    Added Connexion and lists retrivement

---
+++ SPOMig/SPOLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SPOMig
+{
+    class SPOLogic
+    {
+        public ClientContext Context { get; set; }
+
+        public SPOLogic(ClientContext ctx)
+        {
+            this.Context = ctx;
+        }
+
+        public ListCollection getLists()
+        {
+            using (Context)
+            {
+                ListCollection Libraries = Context.Web.Lists;
+                Context.Load(Libraries);
+                Context.ExecuteQuery();
+                return Libraries;
+            }
+        }
+    }
+}
